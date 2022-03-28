76d08003d08b5f88f24414d64a46562c2772e832
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2022-03-27 21:47:34 -0500

    Delete installer directory

--- installer/update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-DEL "Medicat Installer.exe"
-DEL "MediCat_Installer.exe"
-REN MEDICAT_NEW.EXE "Medicat Installer.exe"
-cls
-start "Medicat Installer.exe" "Medicat Installer.exe"
-del %0 && exit
