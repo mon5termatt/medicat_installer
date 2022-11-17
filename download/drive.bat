2fe5a6800167d31ecac22753883e3ae0f58e28ee
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2022-11-17 09:05:52 -0600

    Update drive.bat

--- download/drive.bat
+++ download/drive.bat
@@ -8,7 +8,13 @@ set file3=1nhFdNHYiWhsh__uL6h1qo2QMceEcqhEy
 set file4=1uO9I1poXhwJ-FP7n1kFS6NY7na_ZrSZR
 set file5=1ubWGDRP3Cy2bk1yU008TGMQ0zRA2lsVF
 set file6=1k78sLJTUyxW-zu7rwn9crHYZjHjah3OE
-set file7=1b8ZJonZfq9B3UDc1PSAQ2nynE-wRua9z
+set file7=1xQJDoqMx72gnmiIDl5HJoWTWCmLWOr7N
+set file8=1b8ZJonZfq9B3UDc1PSAQ2nynE-wRua9z
+set file9=1ciT345w9PINo95siK05i-gEjYjF0zlwC
+set file10=1CoiChOCUgX5uLxz6SmwR2xPAboQ_wLXv
+set file11=1SJxlEyIGwbOZBr4OV4fdS23xnl0NTgH4
+set file12=1jSYbrQFqg5rhOMWy_kWW_nuGCcZ-VfuK
+
 if exist "%CD%\WPy%bit%-31080\" (goto download)
 wget https://github.com/winpython/winpython/releases/download/5.0.20221030final/Winpython%bit%-3.10.8.0dot.exe
 Winpython%bit%-3.10.8.0dot.exe -y
@@ -25,8 +31,6 @@ gdown %file3% -O %maindir%\MediCat.USB.v21.12.zip.003
 gdown %file4% -O %maindir%\MediCat.USB.v21.12.zip.004
 gdown %file5% -O %maindir%\MediCat.USB.v21.12.zip.005
 gdown %file6% -O %maindir%\MediCat.USB.v21.12.zip.006
-::gdown %file7% -O %maindir%\MediCat.USB.v21.12.zip.001
-echo.
 title Medicat Installer [HASHCHECK]
 cls
 if exist "%CD%\*.001" (echo..001 Exists) else (goto gdriveerror)
@@ -35,16 +39,35 @@ if exist "%CD%\*.003" (echo..003 Exists) else (goto gdriveerror)
 if exist "%CD%\*.004" (echo..004 Exists) else (goto gdriveerror)
 if exist "%CD%\*.005" (echo..005 Exists) else (goto gdriveerror)
 if exist "%CD%\*.006" (echo..006 Exists) else (goto gdriveerror)
-goto hashfiles
+goto verifyfiles
+
+
 :gdriveerror
 echo. Attempting MIRROR dir
-start https://drive.google.com/drive/folders/1R_05cS0Vq24xfuX27j75OiZrC_mZgQUB
-echo.Manually download these
-pause
+gdown %file7% -O %maindir%\MediCat.USB.v21.12.zip.001
+gdown %file8% -O %maindir%\MediCat.USB.v21.12.zip.002
+gdown %file9% -O %maindir%\MediCat.USB.v21.12.zip.003
+gdown %file10% -O %maindir%\MediCat.USB.v21.12.zip.004
+gdown %file11% -O %maindir%\MediCat.USB.v21.12.zip.005
+gdown %file12% -O %maindir%\MediCat.USB.v21.12.zip.006
+echo.
+title Medicat Installer [HASHCHECK]
+cls
+if exist "%CD%\*.001" (echo..001 Exists) else (goto downloadfail)
+if exist "%CD%\*.002" (echo..002 Exists) else (goto downloadfail)
+if exist "%CD%\*.003" (echo..003 Exists) else (goto downloadfail)
+if exist "%CD%\*.004" (echo..004 Exists) else (goto downloadfail)
+if exist "%CD%\*.005" (echo..005 Exists) else (goto downloadfail)
+if exist "%CD%\*.006" (echo..006 Exists) else (goto downloadfail)
 goto verifyfiles
+:downloadfail
+echo.Was unable to download via drive. please try the torrent
+pause
+goto exit
+
+
 :verifyfiles
 wget "http://cdn.medicatusb.com/files/hasher/Google_Drive_Validate_Files.exe" -O ./Google_Drive_Validate_Files.exe -q
 start Google_Drive_Validate_Files.exe Google_Drive_Validate_Files.exe
 :exit
-
 exit
