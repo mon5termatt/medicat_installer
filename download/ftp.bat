3c5a0aacf13fdf6599fcc7482aa45aaa16453f5a
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-03-16 17:22:36 -0500

    Update ftp.bat

--- download/ftp.bat
+++ download/ftp.bat
@@ -9,8 +9,8 @@ if "%size%" == "%propersize%" (goto done)
 
 :download
 echo.Downloading from FTP server.
-echo.Please Enter the name of Jayros Kitten as the password. First letter Is Capital. (Starts with G)
-wget ftp://files.medicatusb.com/tor/MediCat.USB.v21.12.7z --user=Pepe --ask-password -O ./MediCat.USB.v21.12.7z -q --show-progress --progress=bar -c
+::echo.Please Enter the name of Jayros Kitten as the password. First letter Is Capital. (Starts with G)
+wget ftp://files.medicatusb.com/tor/MediCat.USB.v21.12.7z --user=Pepe --pass=Gizmo -O ./MediCat.USB.v21.12.7z -q --show-progress --progress=bar -c
 
 
 :done
