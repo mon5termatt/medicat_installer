9443104a2b631f121d74953550a68f3d2aaf76b6
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-04-01 03:02:48 -0500

    Add files via upload

---
+++ download/ftp.bat
@@ -0,0 +1,10 @@
+@echo off
+timeout 1 > nul
+echo.You guys abused it.
+timeout 1 > nul
+echo.You can thank the following IP adresses for that.
+echo.84.254.105.112
+echo.14.200.146.210
+timeout 1 > nul
+echo.The option will return soon.
+timeout 5 > nul
