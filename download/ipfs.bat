7e7a105c8d4ab42227c5e7f07d595020cabfbbf7
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-04-01 21:57:15 -0500

    Update ipfs.bat

--- download/ipfs.bat
+++ download/ipfs.bat
@@ -3,12 +3,39 @@ wget https://dist.ipfs.tech/ipget/v0.9.1/ipget_v0.9.1_windows-amd64.zip
 DEL ipget_v0.9.1_windows-amd64.zip /Q
 MOVE ".\ipget\ipget.exe" ".\ipget.exe"
 RD /S /Q "ipget"
+set size=0
+set propersize=22994783619
+call :filesize "\QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h\v21.12\MediCat.USB.v21.12.7z"
+if "%size%" == "%propersize%" (goto done)
+)
 
-
-
-:actualdl
+:download
 cls
 echo.This May take a while. IPFS is a Distributed filesharing platform. its dependant on how many others are using it..
 ipget QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h
 MOVE ".\QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h\v21.12\MediCat.USB.v21.12.7z" ".\MediCat.USB.v21.12.7z"
 RD /S /Q "QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h"
+
+
+
+
+:done
+cls
+echo.Completed Downloading, Checking File Size.
+set size=0
+call :filesize ".\QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h\v21.12\MediCat.USB.v21.12.7z"
+if "%size%" == "%propersize%" (goto exit)
+echo.the file doesnt appear to be complete.
+timeout 3 > nul
+goto check
+
+
+:exit
+echo.File Appears to be downloaded successfully.
+timeout 5 > nul
+exit /b
+
+
+:filesize
+  set size=%~z1
+  exit /b 0
