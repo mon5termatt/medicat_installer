949c243fcd9718a8152c455cdfc9bd6e8f3e4ab3
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-04-23 23:27:31 -0500

    Update ipfs.bat

--- download/ipfs.bat
+++ download/ipfs.bat
@@ -1,23 +1,20 @@
 @echo off
-wget https://dist.ipfs.tech/ipget/v0.9.1/ipget_v0.9.1_windows-amd64.zip
+wget https://dist.ipfs.tech/ipget/v0.9.1/ipget_v0.9.1_windows-amd64.zip -q
 7z x ipget_v0.9.1_windows-amd64.zip
 DEL ipget_v0.9.1_windows-amd64.zip /Q
 MOVE ".\ipget\ipget.exe" ".\ipget.exe"
 RD /S /Q "ipget"
-
 :check
 set size=0
 set propersize=22994783619
-call :filesize "\QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h\v21.12\MediCat.USB.v21.12.7z"
+call :filesize "MediCat.USB.v21.12.7z"
 if "%size%" == "%propersize%" (goto done)
 )
 
 :download
 cls
 echo.This May take a while. IPFS is a Distributed filesharing platform. its dependant on how many others are using it..
-ipget QmZ4c4QuujGK2e4kyL975Hg2LLAzsgbzq7CbuYAvAzrAhb
-::MOVE ".\QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h\v21.12\MediCat.USB.v21.12.7z" ".\MediCat.USB.v21.12.7z"
-::RD /S /Q "QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h"
+ipget QmZ4c4QuujGK2e4kyL975Hg2LLAzsgbzq7CbuYAvAzrAhb -o MediCat.USB.v21.12.7z --progress
 
 
 
