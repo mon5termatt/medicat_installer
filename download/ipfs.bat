d9ca6dff5878f67c5eb50b73fd91c5abaf33bc4e
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-04-23 23:20:01 -0500

    Update ipfs.bat

--- download/ipfs.bat
+++ download/ipfs.bat
@@ -1,8 +1,11 @@
+@echo off
 wget https://dist.ipfs.tech/ipget/v0.9.1/ipget_v0.9.1_windows-amd64.zip
 7z x ipget_v0.9.1_windows-amd64.zip
 DEL ipget_v0.9.1_windows-amd64.zip /Q
 MOVE ".\ipget\ipget.exe" ".\ipget.exe"
 RD /S /Q "ipget"
+
+:check
 set size=0
 set propersize=22994783619
 call :filesize "\QmVRySjFZ5xFAjQ6waAseMw8DfiDkJWsgdyxR5Uc4W215h\v21.12\MediCat.USB.v21.12.7z"
