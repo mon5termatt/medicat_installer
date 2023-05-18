6d3ca6006f168f963174633ca79a59dab257f5d9
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-05-17 21:24:53 -0400

    Update CheckFiles.bat

--- hasher/CheckFiles.bat
+++ hasher/CheckFiles.bat
@@ -2,8 +2,5 @@
 cd /d %~dp0
 wget "https://raw.githubusercontent.com/mon5termatt/medicat_installer/main/hasher/MedicatFiles.md5" -O ./MedicatFiles.md5 -q
 echo.Now Checking files.
-echo.once you see "29652 files checked" it is done.
-echo.you can close the quicksfv program at any time to skip the check.
-QuickSFV.EXE MedicatFiles.md5
-del MedicatFiles.md5
+start QuickSFV MedicatFiles.md5
 del %0 && exit
