164a85dabd949e7965854b65c81dc68474f6baa8
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-06-14 23:05:41 -0500

    Update CheckFiles.bat

--- hasher/CheckFiles.bat
+++ hasher/CheckFiles.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-wget "https://raw.githubusercontent.com/mon5termatt/medicat_installer/main/hasher/MedicatFiles.md5" -O ./MedicatFiles.md5 -q
+curl "https://raw.githubusercontent.com/mon5termatt/medicat_installer/main/hasher/MedicatFiles.md5" -o  ./MedicatFiles.md5 -s -L
 echo.Now Checking files.
 start QuickSFV MedicatFiles.md5
 del %0 && exit
