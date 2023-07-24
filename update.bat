66b63eeae0edb82dbc35c2571505b73b0c0dfa77
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-07-24 17:45:27 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -6,8 +6,7 @@ DEL "Medicat Installer.bat"
 DEL "MediCat_Installer.bat"
 cls
 echo.Updating Medicat Installer. Please wait while the new file is downloaded.
-:: FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK 
-curl https://github.com/mon5termatt/medicat_installer/releases/download/3506/Medicat_Installer.bat -o Medicat_Installer.bat -q -L
+curl http://url.medicatusb.com/installerupdate -o Medicat_Installer.bat -q -L
 cls
 start cmd /k Medicat_Installer.bat Medicat_Installer.bat
 del %0 && exit
