325273ee2c19f4d99e16357a1700a1e4f1005e09
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-08-04 00:10:13 -0500

    Update cdn.bat

--- download/cdn.bat
+++ download/cdn.bat
@@ -14,17 +14,17 @@ echo Testing Server 2 - medicat.itrio.xyz
 FOR /F %%I in ('curl -e https://installer.medicatusb.com --max-time 3 "https://mirrors.itrio.xyz/unpacked/MediCat.USB.v21.12.7z" -o server2.7z -s -w "%%{speed_download}"') do set server2=%%I
 set /a server2=%server2% / 1000000
 echo.%server2%mbps
-::echo Testing Server 3 - 
-::FOR /F %%I in ('curl -e https://installer.medicatusb.com --max-time 3 "" -o server3.7z -s -w "%%{speed_download}"') do set server3=%%I
-::set /a server3=%server3% / 1000000
-::echo.%server3%mbps
+echo Testing Server 3 - files.dog
+FOR /F %%I in ('curl -e https://installer.medicatusb.com --max-time 3 "https://files.dog/OD%20Rips/MediCat/v21.12/MediCat.USB.v21.12.7z" -o server3.7z -s -w "%%{speed_download}"') do set server3=%%I
+set /a server3=%server3% / 1000000
+echo.%server3%mbps
 
 
 
 timeout 1 >nul
 del server1.7z /q
 del server2.7z /q
-::del server3.7z /q
+del server3.7z /q
 
 if %server1% geq %server2% (
     if %server1% geq %server3% (
@@ -36,11 +36,11 @@ if %server2% geq %server1% (
         set url="https://mirrors.itrio.xyz/unpacked/MediCat.USB.v21.12.7z" & goto download
     )
 )
-::if %server3% geq %server1% (
-::    if %server3% geq %server2% (
-::        set url="https://mirror.fangshdow.trade/medicat-usb/MediCat%%20USB%%20v21.12/MediCat.USB.v21.12.7z" & goto download
-::    )
-::)
+if %server3% geq %server1% (
+    if %server3% geq %server2% (
+        set url="https://mirror.fangshdow.trade/medicat-usb/MediCat%%20USB%%20v21.12/MediCat.USB.v21.12.7z" & goto download
+    )
+)
 
 :download
 echo.Downloading from fastest server.
