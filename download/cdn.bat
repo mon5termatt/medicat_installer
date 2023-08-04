2d2316b194dd02013f8c622dafb740d5aac58b25
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-08-04 00:13:14 -0500

    Update cdn.bat

--- download/cdn.bat
+++ download/cdn.bat
@@ -15,7 +15,7 @@ FOR /F %%I in ('curl -e https://installer.medicatusb.com --max-time 3 "https://m
 set /a server2=%server2% / 1000000
 echo.%server2%mbps
 echo Testing Server 3 - files.dog
-FOR /F %%I in ('curl -e https://installer.medicatusb.com --max-time 3 "https://files.dog/OD%20Rips/MediCat/v21.12/MediCat.USB.v21.12.7z" -o server3.7z -s -w "%%{speed_download}"') do set server3=%%I
+FOR /F %%I in ('curl -e https://installer.medicatusb.com --max-time 3 "https://files.dog/OD%%20Rips/MediCat/v21.12/MediCat.USB.v21.12.7z" -o server3.7z -s -w "%%{speed_download}"') do set server3=%%I
 set /a server3=%server3% / 1000000
 echo.%server3%mbps
 
@@ -38,7 +38,7 @@ if %server2% geq %server1% (
 )
 if %server3% geq %server1% (
     if %server3% geq %server2% (
-        set url="https://mirror.fangshdow.trade/medicat-usb/MediCat%%20USB%%20v21.12/MediCat.USB.v21.12.7z" & goto download
+        set url="https://files.dog/OD%%20Rips/MediCat/v21.12/MediCat.USB.v21.12.7z" & goto download
     )
 )
 
