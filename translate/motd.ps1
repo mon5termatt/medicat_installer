45e39b1d406a667197099b749845d84c6314524c
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2023-11-07 21:12:15 -0600

    Update motd.ps1

--- translate/motd.ps1
+++ translate/motd.ps1
@@ -1,9 +1,10 @@
 ﻿$TargetLanguage = $Args[0]
 
-$text1 = "if you have any issues please contact the discord"
+$text1 = "We are currently doing a giveaway in the discord."
 
 $Uri1 = “https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$text1”
 $Response = Invoke-RestMethod -Uri $Uri1 -Method Get
 $Translation1 = $Response[0].SyncRoot | foreach { $_[0] }
 
 write-host $Translation1
+write-host "https://discord.gg/medicat"
