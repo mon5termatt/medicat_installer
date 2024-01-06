7c414b02b6ae545eabc9d5d6ea0025bdb416d2fa
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2024-01-05 19:07:55 -0600

    Update motd.ps1

--- translate/motd.ps1
+++ translate/motd.ps1
@@ -1,6 +1,6 @@
 ﻿$TargetLanguage = $Args[0]
 
-$text1 = "We are currently doing a giveaway in the discord."
+$text1 = "Happy New Year."
 
 $Uri1 = “https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$text1”
 $Response = Invoke-RestMethod -Uri $Uri1 -Method Get
