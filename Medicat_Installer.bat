fdcd6c0599103bf59df9c527c400c38f82626dc4
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2024-02-26 12:29:08 -0600

    Update Medicat_Installer.bat

--- Medicat_Installer.bat
+++ Medicat_Installer.bat
@@ -3,7 +3,7 @@ title Medicat Installer [STARTING]
 cd /d %~dp0
 Set "Path=%Path%;%CD%;%CD%\bin;"
 set maindir=%CD%
-set localver=3515
+set localver=3516
 set medicatver=21.12
 set installertext=[31mM[32mE[33mD[34mI[35mC[36mA[31mT[32m I[33mN[34mS[35mT[36mA[31mL[32mL[33mE[34mR[0m
 set format=Yes
