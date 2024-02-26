19b023ee706da45ad4f54659ddb6935ebc8c08e9
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2024-02-25 19:37:38 -0600

    Update motd.ps1

--- translate/motd.ps1
+++ translate/motd.ps1
@@ -2,7 +2,7 @@ $TargetLanguage = $Args[0]
 
 $text1 = "Giveaway in the discord"
 
-$Uri1 = “https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$text1”
+$Uri1 = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$text1"
 $Response = Invoke-RestMethod -Uri $Uri1 -Method Get
 $Translation1 = $Response[0].SyncRoot | foreach { $_[0] }
 
