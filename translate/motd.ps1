aa21cb2785c415a76196f27c625817ec64fdbeed
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2025-02-05 19:53:00 -0600

    RIP Novaspirit

--- translate/motd.ps1
+++ translate/motd.ps1
@@ -1,10 +1,11 @@
 $TargetLanguage = $Args[0]
 
-$text1 = "Potential update soon, I swear."
+$text1 = "Rest in peace Novaspirit Tech. Thanks for the vids."
 
 $Uri1 = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$text1"
 $Response = Invoke-RestMethod -Uri $Uri1 -Method Get
 $Translation1 = $Response[0].SyncRoot | foreach { $_[0] }
 
 write-host $Translation1
-write-host "https://discord.gg/medicat"
+write-host "~ Matt"
+### write-host "https://discord.gg/medicat"
