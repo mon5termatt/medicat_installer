5d47b8beadddb47aa788135f024e849e103ae877
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2025-06-08 00:53:24 -0500

    Update motd.ps1

--- translate/motd.ps1
+++ translate/motd.ps1
@@ -3,7 +3,6 @@ $today = Get-Date
 
 # Check Holiday
 # https://nationaltoday.com/category/world/
-# Next Up Easter (4/20)
 
 if ($today.Month -eq 4 -and $today.Day -eq 1) {
 # Holiday Text (And/Or Action)
@@ -11,9 +10,10 @@ $text1 = "Happy April Fools Day"
 Start-Process "https://coin.medicatusb.com/"
 } else {
 # Normal Text (Updated every once in a while.)
-$text1 = "If you or a loved one has been diagnosed with a broken computer, you may be asked to help now..."
+$text1 = "Happy Pride Month."
 } 
-
+# This one is gold, save it. 
+# If you or a loved one has been diagnosed with a broken computer, you may be asked to help now...
 $Uri1 = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$text1"
 $Response = Invoke-RestMethod -Uri $Uri1 -Method Get
 $Translation1 = $Response[0].SyncRoot | foreach { $_[0] }
