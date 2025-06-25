cc51bf0276a8fe5133ee7481d373c6eba62ee09b
Ludovic <56892223+Ludo-code@users.noreply.github.com>
2025-06-25 20:55:30 +0200

    Fix dep for fedora
    Fixing p7zip for fedora because p7zip-full is not in fedora packages.

--- Medicat_Installer.sh
+++ Medicat_Installer.sh
@@ -22,7 +22,7 @@ wget["default"]="wget"
 declare -A zip
 zip["arch"]="p7zip"
 zip["nixos"]="nixos.p7zip"
-zip["fedora"]="p7zip-full p7zip-plugins"
+zip["fedora"]="p7zip p7zip-plugins"
 zip["nobara"]="p7zip-full p7zip-plugins"
 zip["centos"]="p7zip p7zip-plugins"
 zip["alpine"]="7zip"
