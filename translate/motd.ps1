cbe8c9ee9a6606c48df1c6ba07276c9e22a87b9c
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2025-07-09 20:08:24 -0500

    Update motd.ps1

--- translate/motd.ps1
+++ translate/motd.ps1
@@ -7,16 +7,20 @@ $today = Get-Date
 if ($today.Month -eq 4 -and $today.Day -eq 1) {
 # Holiday Text (And/Or Action)
 $text1 = "Happy April Fools Day"    
-Start-Process "https://coin.medicatusb.com/"
+# Start-Process "https://coin.medicatusb.com/"
 } else {
+
 # Normal Text (Updated every once in a while.)
 $text1 = "If you or a loved one has been diagnosed with a broken computer, you may be asked to help now..."
-} 
+} # Close Loop
+
+### Saved messages
 # This one is gold, save it. 
 # If you or a loved one has been diagnosed with a broken computer, you may be asked to help now...
+
+# Do Translation
 $Uri1 = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=$($TargetLanguage)&dt=t&q=$text1"
 $Response = Invoke-RestMethod -Uri $Uri1 -Method Get
 $Translation1 = $Response[0].SyncRoot | foreach { $_[0] }
 
 write-host $Translation1
-#write-host "https://discord.gg/medicat"    
